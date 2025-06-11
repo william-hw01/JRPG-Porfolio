8c00d4c208a75f4d7bc6eaef862d4f5966fed404
huenwat01 <winghuenwat@gmail.com>
2025-06-11 16:15:32 +0800

    Update game

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './style.css';
+// Remove this import since we're linking CSS directly in HTML
+// import './style.css';
 import Phaser from 'phaser';
 
 const size = {
@@ -11,6 +12,16 @@ const config = {
   width: size.width,
   height: size.height,
   canvas: document.getElementById('gameCanvas'),
+  // Add basic scene configuration
+  scene: {
+    preload: function() {},
+    create: function() {
+      this.add.text(100, 100, "Game Loaded!", { 
+        font: "32px Arial", 
+        fill: "#ffffff" 
+      });
+    }
+  }
 };
 
 const game = new Phaser.Game(config);
