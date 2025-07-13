d3a99c8903a9bee32599d9e508daca004164ed7f
huenwat01 <winghuenwat@gmail.com>
2025-07-13 22:33:06 +0800

    Version 0.3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Phaser from 'phaser';
 import './style.css'; 
 import { displayDialogue, displayHint } from './utils.js';
+import portfolioRM from './portfolioRM.js';
+
 
 const size = {
   width: 900,
@@ -9,15 +11,14 @@ const size = {
 
 class GameScene extends Phaser.Scene {
   constructor() {
-    super();
+    super({ key: 'GameScene' });
     this.pcCooldown = false;
     this.cooldownTime = 2000; 
     this.isHintVisible = false;
     this.currentHintObject = null;
   }
-
+  
   preload() {
-    this.load.image('spawnPT64', 'map/spawnPT/spawnPT64.png');
     this.load.image('protagonist_64x64', 'character/player/protagonist_64x64.png');
     this.load.json('playerStatus', 'character/player/playerStatus.json');
     this.load.json('dialogue', 'character/player/dialogue.json');
@@ -26,6 +27,14 @@ class GameScene extends Phaser.Scene {
   }
 
   create() {
+    //debug purpose
+    //this.scene.start('portfolioRM');
+    //this.physics.world.createDebugGraphic();
+
+    // Set player status in registry to loaded JSON data
+    const playerStatus = this.cache.json.get('playerStatus');
+    this.registry.set('playerStatus', playerStatus);
+    const Pstatus = this.registry.get('playerStatus');
     // Key bindings
     this.keys = {
       W: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
@@ -42,15 +51,24 @@ class GameScene extends Phaser.Scene {
     const decoration = map.createLayer('decoration', tileset, -125, -80).setScale(0.75);
     const spawnPTObj = map.getObjectLayer('spawnPT').objects;
 
-    // Player setup with scaled hitbox
-    this.player = this.physics.add.image(size.width/2, size.height/2, 'protagonist_64x64')
+    // Player setup at spawnPT location from map
+    let spawnX, spawnY;
+    if(Pstatus.intro) {
+      spawnX = size.width / 2;
+      spawnY = size.height / 2;
+    } else {
+      const spawnPoint = spawnPTObj.find(obj => obj.name === 'spawnPT');
+      spawnX = spawnPoint ? spawnPoint.x * 0.75 - 125 : size.width / 2;
+      spawnY = spawnPoint ? spawnPoint.y * 0.75 - 80 : size.height / 2;
+    }
+    this.player = this.physics.add.image(spawnX, spawnY, 'protagonist_64x64')
       .setScale(1)
       .setDepth(1000)
       .setSize(55, 70) // Scaled hitbox (65 * 1, 70 * 1)
       .setOffset(5, 0); // Scaled offset (2 * 1)
     this.player.setCollideWorldBounds(true);
 
-    this.inDialogue = true;
+    this.inDialogue = Pstatus.intro;
     this.cursor = this.input.keyboard.createCursorKeys();
 
     // Tilemap collisions
@@ -62,7 +80,7 @@ class GameScene extends Phaser.Scene {
     // PC object setup with scaled collision
     this.interactables = this.physics.add.staticGroup();
     this.pcGroup = this.physics.add.staticGroup(); // Separate group for PC
-
+    this.portfolioRM = this.physics.add.staticGroup();
     spawnPTObj.forEach(obj => {
       const x = obj.x * 0.75 - 125;
       const y = obj.y * 0.75 - 80;
@@ -77,6 +95,14 @@ class GameScene extends Phaser.Scene {
           .setVisible(false)
           .refreshBody();
 
+
+      } else if (obj.name === 'portfolioRM') {
+        // Add portfolioRM to its own group with collision
+        const hitbox = this.portfolioRM.create(x + width + 0.9*width  , y + height / 2, null)
+          .setScale(6)
+          .setData('type', obj.name)
+          .setVisible(false)
+          .refreshBody();
           
       } else {
         // Add other interactables (e.g., PC_Hint) to the general group with overlap
@@ -100,6 +126,15 @@ class GameScene extends Phaser.Scene {
       }
     });
 
+    this.physics.add.collider(this.player, this.portfolioRM, (player, obj) => {
+      if (!this.pcCooldown && !this.inDialogue && obj.getData('type') === 'portfolioRM') {
+        console.log('portfolioRM collision detected');
+        //displayHint("github info", false);
+        this.onPortfolioRMCollision();
+      }
+    });
+
+
     // Overlap for other interactables (e.g., PC_Hint)
     this.physics.add.overlap(this.player, this.interactables, (player, obj) => {
       const type = obj.getData('type');
@@ -113,28 +148,31 @@ class GameScene extends Phaser.Scene {
     }, null, this);
 
 
+    if (Pstatus.intro) {
+      displayDialogue(this.cache.json.get('dialogue').intro, () => {
+        this.inDialogue = false;
+        Pstatus.intro = false;
+        this.registry.set('playerStatus', Pstatus);
+      });
+    }
 
-    displayDialogue(this.cache.json.get('dialogue').intro, () => {
-      this.inDialogue = false;
-    });
 
-    this.physics.world.createDebugGraphic();
   }
 
   update() { 
-    const playerStatus = this.cache.json.get('playerStatus');
+    const Pstatus = this.registry.get('playerStatus');
     this.player.setVelocity(0);
     
     if (!this.inDialogue) {
       if (this.cursor.left.isDown || this.keys.A.isDown) {
-        this.player.setVelocityX(-playerStatus.playerSpeed * 0.75);
+        this.player.setVelocityX(-Pstatus.playerSpeed * 0.75);
       } else if (this.cursor.right.isDown || this.keys.D.isDown) {
-        this.player.setVelocityX(playerStatus.playerSpeed * 0.75);
+        this.player.setVelocityX(Pstatus.playerSpeed * 0.75);
       }
       if (this.cursor.up.isDown || this.keys.W.isDown) {
-        this.player.setVelocityY(-playerStatus.playerSpeed * 0.75);
+        this.player.setVelocityY(-Pstatus.playerSpeed * 0.75);
       } else if (this.cursor.down.isDown || this.keys.S.isDown) {
-        this.player.setVelocityY(playerStatus.playerSpeed * 0.75);
+        this.player.setVelocityY(Pstatus.playerSpeed * 0.75);
       }
     }
 
@@ -148,26 +186,27 @@ class GameScene extends Phaser.Scene {
   }
 
   onPCCollision() {
-    
     if (this.pcCooldown || this.inDialogue) return;
-    
     this.pcCooldown = true;
     this.inDialogue = true;
-    
     displayDialogue(this.cache.json.get('dialogue').PC, () => {
       this.inDialogue = false;
     });
-
     this.time.delayedCall(this.cooldownTime, () => {
       this.pcCooldown = false;
     });
     
   }
 
+  onPortfolioRMCollision() {
+    this.scene.start('portfolioRM');
+  }
+
   onPCHintCollision() {
     displayHint("github info");
   }
 }
+export default GameScene;
 
 const config = {
   type: Phaser.WEBGL,
@@ -176,7 +215,7 @@ const config = {
   canvas: document.getElementById('gameCanvas'),
   transparent: true,
   backgroundColor: '#FFFFFF',
-  scene: [GameScene],
+  scene: [GameScene, portfolioRM],
   physics: { 
     default: 'arcade',
     arcade: {
---
+++ src/portfolioRM.js
@@ -0,0 +1,225 @@
+import Phaser from 'phaser'; 
+import { displayDialogue, displayHint } from './utils.js';
+import GameScene from './main.js';
+
+class portfolioRM extends Phaser.Scene {
+    constructor() {
+        super({ key: 'portfolioRM' });
+        this.Cooldown = false;
+        this.cooldownTime = 2000; 
+        this.isHintVisible = false;
+        this.currentHintObject = null;
+    }
+
+    preload() {
+        this.load.image('protagonist_64x64', 'character/player/protagonist_64x64.png');
+        this.load.json('playerStatus', 'character/player/playerStatus.json');
+        this.load.json('dialogue', 'character/player/dialogue.json');
+        this.load.image('spreadsheet', 'spreadsheet64x64.png');
+        this.load.tilemapTiledJSON('portfolioRM', 'map/portfolioRM/portfolioRM.json');
+    }
+
+    create() {
+        //display hitbox
+        //this.physics.world.createDebugGraphic();
+        // Key bindings
+        this.keys = {
+            W: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+            A: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+            S: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+            D: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        };
+
+        // Map setup
+        const map = this.add.tilemap('portfolioRM');
+        const tileset = map.addTilesetImage('spreadsheet64x64', 'spreadsheet');
+        const portfolioRM = map.createLayer('portfolioRM', tileset, -125, -80).setScale(0.75);
+        const border = map.createLayer('border', tileset, -125, -80).setScale(0.75);  
+        const decoration = map.createLayer('decoration', tileset, -125, -80).setScale(0.75);
+        const RMobj = map.getObjectLayer('obj').objects;
+
+        // Player setup at spawnPT location from map
+        const spawnPoint = RMobj.find(obj => obj.name === 'spawnPT');
+        const spawnX = spawnPoint ? spawnPoint.x * 0.75 - 125 : size.width / 2;
+        const spawnY = spawnPoint ? spawnPoint.y * 0.75 - 80 : size.height / 2;
+        this.player = this.physics.add.image(spawnX, spawnY, 'protagonist_64x64')
+        .setScale(1)
+        .setDepth(1000)
+        .setSize(55, 70) // Scaled hitbox (65 * 1, 70 * 1)
+        .setOffset(5, 0); // Scaled offset (2 * 1)
+        this.player.setCollideWorldBounds(true);
+
+        this.cursor = this.input.keyboard.createCursorKeys();
+
+        // Tilemap collisions
+        this.physics.add.collider(this.player, border);
+        border.setCollisionBetween(1, 100, true, true);
+        this.physics.add.collider(this.player, decoration);
+        decoration.setCollisionBetween(1, 100, true, true);
+
+        //obj collisions
+        this.interactables = this.physics.add.staticGroup();
+        this.academic = this.physics.add.staticGroup();
+        this.spawnRM = this.physics.add.staticGroup();
+        this.undeveloped = this.physics.add.staticGroup();
+
+        RMobj.forEach(obj => {
+        const x = obj.x * 0.75 - 125;
+        const y = obj.y * 0.75 - 80;
+        const width = obj.width * 0.75;
+        const height = obj.height * 0.75;
+        
+            if (obj.name === 'academic') {
+                // Add academic to its own group with collision
+                const hitbox = this.academic.create(x + width / 2, y - height / 2 + 10, null)
+                .setScale(1.5)
+                .setData('type', obj.name)
+                .setVisible(false)
+                .refreshBody();
+
+
+            } else if (obj.name === 'JRPGMap') {
+               const hitbox = this.undeveloped.create(x + width / 2, y + 3.4*height , null)
+                .setScale(10.5)
+                .setData('type', obj.name)
+                .setVisible(false)
+                .refreshBody();
+                
+            
+            }else if (obj.name === 'undevelop') {
+               const hitbox = this.undeveloped.create(x + 1.5*width, y + height / 2, null)
+                .setScale(4.5)
+                .setData('type', obj.name)
+                .setVisible(false)
+                .refreshBody(); 
+            } else if (obj.name === 'spawnRM') {
+               const hitbox = this.spawnRM.create(x - 0.5 * width, y + height / 2, null)
+                .setScale(4.5)
+                .setData('type', obj.name)
+                .setVisible(false)
+                .refreshBody();
+            } else {
+                // Add other interactables (e.g., PC_Hint) to the general group with overlap
+                const hitbox = this.interactables.create(x + width / 2, y + height / 2, null)
+                .setSize(width, height)
+                .setData('type', obj.name)
+                .setVisible(false);
+                const debugGraphic = this.add.graphics()
+                .lineStyle(2, 0x00ff00, 0.5) // Different color for non-PC debug
+                .strokeRect(x, y, width, height)
+                .setVisible(false);
+            }
+        });
+
+        this.physics.add.collider(this.player, this.academic, (player, obj) => {
+            if (!this.Cooldown && !this.inDialogue && obj.getData('type') === 'academic') {
+                console.log('Academic collision detected');
+                displayHint("academic info", false);
+                this.onAcademicCollision();
+            }
+        });
+
+        this.physics.add.collider(this.player, this.undeveloped, (player, obj) => {
+            if (!this.Cooldown && !this.inDialogue ) {
+                console.log('undeveloped collision detected');
+                this.onUndevelopedCollision();
+            }
+        });
+
+        this.physics.add.collider(this.player, this.spawnRM, (player, obj) => {
+            if (!this.Cooldown && !this.inDialogue ) {
+                console.log('spawnRM collision detected');
+                //displayHint("github info", false);
+                this.onSpawnRMCollision();
+            }
+        });
+
+        // Overlap for other interactables 
+        this.physics.add.overlap(this.player, this.interactables, (player, obj) => {
+            const type = obj.getData('type');
+            if (type === 'academic_hint') {
+                if (!this.isHintVisible) {
+                    this.currentHintObject = obj;
+                    displayHint("academic info", true);
+                    this.isHintVisible = true;
+                }
+            }
+        }, null, this);
+
+        this.physics.add.overlap(this.player, this.interactables, (player, obj) => {
+            const type = obj.getData('type');
+            if (type === 'JRPG_hint') {
+                if (!this.isHintVisible) {
+                    this.currentHintObject = obj;
+                    displayHint("Moving forward will start the JRPG(InDevelopment)", true);
+                    this.isHintVisible = true;
+                }
+            }
+        }, null, this);
+
+
+    }
+
+    update() {
+        const Pstatus = this.registry.get('playerStatus');
+        this.player.setVelocity(0);
+        
+        if (!this.inDialogue) {
+        if (this.cursor.left.isDown || this.keys.A.isDown) {
+            this.player.setVelocityX(-Pstatus.playerSpeed * 0.75);
+        } else if (this.cursor.right.isDown || this.keys.D.isDown) {
+            this.player.setVelocityX(Pstatus.playerSpeed * 0.75);
+        }
+        if (this.cursor.up.isDown || this.keys.W.isDown) {
+            this.player.setVelocityY(-Pstatus.playerSpeed * 0.75);
+        } else if (this.cursor.down.isDown || this.keys.S.isDown) {
+            this.player.setVelocityY(Pstatus.playerSpeed * 0.75);
+        }
+        }
+
+        if (this.isHintVisible && !this.physics.world.overlap(this.player, this.currentHintObject)) {
+            const type = this.currentHintObject.getData('type');
+            if (type === 'academic_hint') {
+                displayHint("academic info", false);
+            } else if (type === 'JRPG_hint') {
+                displayHint("Moving forward will start the JRPG(InDevelopment)", false);
+            }
+            this.isHintVisible = false;
+            this.currentHintObject = null;
+        }
+    }
+
+    onAcademicCollision() {
+        if (this.Cooldown || this.inDialogue) return;
+        displayHint("academic info", false);
+        this.Cooldown = true;
+        this.inDialogue = true;
+        displayDialogue(this.cache.json.get('dialogue').academic, () => {
+        this.inDialogue = false;
+        });
+        this.time.delayedCall(this.cooldownTime, () => {
+        this.Cooldown = false;
+        });
+  }
+
+  
+  onUndevelopedCollision() {
+    if (this.Cooldown || this.inDialogue) return;
+    this.Cooldown = true;
+    this.inDialogue = true;
+    displayHint("Moving forward will start the JRPG(InDevelopment)", false);
+    displayDialogue(this.cache.json.get('dialogue').inDevelopment, () => {
+        this.inDialogue = false;
+    });
+    this.time.delayedCall(this.cooldownTime, () => {
+        this.Cooldown = false;
+    });
+  }
+
+  onSpawnRMCollision(){
+    this.scene.start('GameScene');
+  }
+}
+
+export default portfolioRM;
+
